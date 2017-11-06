4bc51fb599dca5746216624199d20015d5bcc702
burstein <burstein@dhcp-wifi-8021x-155-41-60-75.bu.edu>
2017-11-06 12:46:09 -0500

    Initialized with my website

---
+++ js/alan.js
@@ -0,0 +1,34 @@
+var ALAN={
+
+}
+ALAN.TurnDownForWhat=function(){
+  window.scrollTo(0, 0);
+	var a = document.createElement("script");
+	a.setAttribute("src", "js/tdfw.js"),
+	document.body.appendChild(a)
+}
+ALAN.imageSwitched=function(){
+	imageLocation='/img/pics/'
+	images=['animatedClouds.gif','prettyLights.jpg','animatedLights.gif']
+	counter=Math.floor(Math.random() * images.length)
+	displayNextImage()
+	startTimer()
+	function startTimer() {
+
+              setInterval(displayNextImage, 10000);
+
+    }
+    function displayNextImage() {
+    		console.log(counter)
+              counter=counter+1
+              counter=counter%images.length
+              console.log("url("+imageLocation+images[counter]+") no-repeat top center;")
+              console.log(document.getElementById('intro'))
+              document.getElementById('intro').style.background='url('+imageLocation+images[counter]+')'+'no-repeat top center'
+          	document.getElementById('intro').style.backgroundSize= 'cover'
+
+    //       $("#intro").fadeOut(1000, function() {
+        
+    // });
+          }
+}
